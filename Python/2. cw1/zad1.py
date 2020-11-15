947916cf7b6388f0c9f59ea4f6df29314d30d758
Artur Węgrodzki <artur.wegrodzki@gmail.com>
2020-11-15 18:12:28 +0100

    zadanie 4

--- Python/2. cw1/zad1.py
+++ Python/2. cw1/zad1.py
@@ -1,6 +1,8 @@
 import pandas as pd
 import numpy as np
 import statistics
+from scipy import mean
+import matplotlib.pyplot as plt
 
 print('zadanie 1\n')
 data1 = pd.read_csv('2. cw1\\MDR_RR_TB_burden_estimates_2020-10-29.csv')
@@ -26,5 +28,27 @@ print('variance(): ' + str(statistics.variance(data2)))
 
 #Same for pvariance and variance
 
-#zadanie 3
+print('\n\nZadanie 4\n')
+data4 = pd.read_csv('2. cw1\\brain_size.csv', sep=';')
 
+print('VIQ mean: '+ str( data4['VIQ'].mean()))
+
+print(data4['Gender'].value_counts())
+
+data4['VIQ'].hist()
+plt.show()
+
+data4['PIQ'].hist()
+plt.show()
+
+data4['FSIQ'].hist()
+plt.show()
+
+data4[data4['Gender'] == 'Female']['VIQ'].hist()
+plt.show()
+
+data4[data4['Gender'] == 'Female']['PIQ'].hist()
+plt.show()
+
+data4[data4['Gender'] == 'Female']['FSIQ'].hist()
+plt.show()
